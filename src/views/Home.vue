530cd93f946d170f3994c6e517ad4e37c7fbddcc
kamaboko <nerorubymoon@gmail.com>
2019-07-30 00:12:49 +0900

    dispray position

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>緯度: {{ longitude }}</p>
+    <p>経度: {{ latitude }}</p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  public latitude: number = 0;
+  public longitude: number = 0;
+
+  /**
+   * getCurrentPosition
+   * infrastructure層に移動予定
+   */
+  public getCurrentPosition() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function error(err: PositionError) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (pos: Position) => {
+        const crd = pos.coords;
+        this.latitude = crd.latitude;
+        this.longitude = crd.longitude;
+      },
+      error,
+      options
+    );
+  }
+  public mounted() {
+    this.getCurrentPosition();
+  }
+}
 </script>
